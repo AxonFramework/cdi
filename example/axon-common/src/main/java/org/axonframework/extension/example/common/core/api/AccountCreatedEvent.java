a6adca59351b5777dc03bfc46fa4e1e639820c32
Reza Rahman <reza_rahman@lycos.com>
2018-08-22 19:04:45 -0400

    Cleanup.

--- example/axon-common/src/main/java/org/axonframework/extension/example/common/core/api/AccountCreatedEvent.java
+++
@@ -1,9 +0,0 @@
-package org.axonframework.extension.example.common.core.api;
-
-import lombok.Value;
-
-@Value
-public class AccountCreatedEvent {
-  String accountId;
-  Integer overdraftLimit;
-}
--- example/axon-common/src/main/java/org/axonframework/extension/example/common/core/api/MoneyWithdrawnEvent.java
+++
@@ -1,9 +0,0 @@
-package org.axonframework.extension.example.common.core.api;
-
-import lombok.Value;
-
-@Value
-public class MoneyWithdrawnEvent {
-  String accountId;
-  Integer amount;
-}
