5491293bceb71f85e13d4a30733903ac02e893db
Milan Savic <milan.savic@axoniq.io>
2018-08-27 14:55:35 +0200

    Reverted example.

--- example/javase/src/main/java/org/axonframework/cdi/example/javase/configuration/AxonConfiguration.java
+++ example/javase/src/main/java/org/axonframework/cdi/example/javase/configuration/AxonConfiguration.java
@@ -4,13 +4,6 @@ import java.io.Serializable;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
 
-import org.axonframework.cdi.example.javase.saga.MySaga;
-import org.axonframework.config.SagaConfiguration;
-import org.axonframework.eventhandling.saga.repository.CachingSagaStore;
-import org.axonframework.eventhandling.saga.repository.SagaStore;
-import org.axonframework.eventhandling.saga.repository.inmemory.InMemorySagaStore;
-import org.axonframework.eventhandling.saga.repository.jdbc.JdbcSagaStore;
-import org.axonframework.eventhandling.saga.repository.jpa.JpaSagaStore;
 import org.axonframework.eventsourcing.eventstore.EventStorageEngine;
 import org.axonframework.eventsourcing.eventstore.inmemory.InMemoryEventStorageEngine;
 
@@ -28,21 +21,5 @@ public class AxonConfiguration implements Serializable {
     @ApplicationScoped
     public EventStorageEngine eventStorageEngine() {
         return new InMemoryEventStorageEngine();
-    }
-
-    @Produces
-    @ApplicationScoped
-    public SagaStore mySaga1Store() {
-        InMemorySagaStore inMemorySagaStore = new InMemorySagaStore();
-        System.out.println("mySaga1Store" + inMemorySagaStore);
-        return inMemorySagaStore;
-    }
-
-    @Produces
-    @ApplicationScoped
-    public SagaStore defaultSagaStore() {
-        JdbcSagaStore jdbcSagaStore = new JdbcSagaStore(null);
-        System.out.println("jdbcSagaStore" + jdbcSagaStore);
-        return jdbcSagaStore;
     }
 }
