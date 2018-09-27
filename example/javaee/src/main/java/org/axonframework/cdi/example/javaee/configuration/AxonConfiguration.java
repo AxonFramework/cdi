7a56790eec2cc5500d45eaed35652d07dc5e9761
Reza Rahman <reza_rahman@lycos.com>
2018-09-27 10:28:45 -0400

    More portable entity manager reference.

--- example/javaee/src/main/java/org/axonframework/cdi/example/javaee/configuration/AxonConfiguration.java
+++ example/javaee/src/main/java/org/axonframework/cdi/example/javaee/configuration/AxonConfiguration.java
@@ -1,10 +1,14 @@
 package org.axonframework.cdi.example.javaee.configuration;
 
 import java.io.Serializable;
+import java.lang.invoke.MethodHandles;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import org.axonframework.cdi.transaction.JtaTransactionManager;
 import org.axonframework.common.jpa.EntityManagerProvider;
 import org.axonframework.common.jpa.SimpleEntityManagerProvider;
@@ -21,9 +25,25 @@ public class AxonConfiguration implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    private static final Logger logger = Logger.getLogger(
+            MethodHandles.lookup().lookupClass().getName());
+
+    /**
+     * Produces the entity manager.
+     *
+     * @return entity manager.
+     */
     @Produces
-    @PersistenceContext(unitName = "account")
-    private EntityManager entityManager;
+    @ApplicationScoped
+    public EntityManager entityManager() {
+        try {
+            return (EntityManager) new InitialContext().lookup("java:comp/env/accounts/entitymanager");
+        } catch (NamingException ex) {
+            logger.log(Level.SEVERE, "Failed to look up entity manager.", ex);
+        }
+
+        return null;
+    }
 
     /**
      * Produces the entity manager provider.
